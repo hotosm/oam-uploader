0118c3c3213e5e7a0f5fe65b3acd113598ebe362
Anand Thakker <vestibule@anandthakker.net>
2015-08-28 15:35:06 -0400

    Actually render status

--- app/assets/scripts/views/status.js
+++ app/assets/scripts/views/status.js
@@ -2,11 +2,18 @@
 var React = require('react/addons');
 var Router = require('react-router');
 var RouteHandler = Router.RouteHandler;
+var moment = require('moment');
+var titlecase = require('titlecase');
 
 var util = require('util');
 var url = require('url');
 var nets = require('nets');
 var apiUrl = require('../config.js').OAMUploaderApi;
+
+function dateFormat (date) {
+  // http://momentjs.com/docs/#/displaying/
+  return moment(date).format('YYYY-M-D [at] H:mm')
+}
 
 var App = module.exports = React.createClass({
   displayName: 'Status',
@@ -40,14 +47,85 @@ var App = module.exports = React.createClass({
           data: data
         });
       } catch (err) {
+        console.error(err);
         return this.setState({
           loading: false,
           errored: true,
           message: 'Error parsing API response; statusCode: ' + resp.statusCode,
-          data: body
+          data: '' + body
         })
       }
     }.bind(this));
+  },
+
+  renderScene: function (scene) {
+    return (
+      <section className="panel status-panel">
+        <header className="panel-header">
+          <div className="panel-headline">
+            <h1 className="panel-title">Scene: <span className="given-title">{scene.title}</span></h1>
+          </div>
+        </header>
+        <div className="panel-body">
+          <dl className="status-details">
+            <dt>Platform</dt>
+            <dd>{scene.platform}</dd>
+            <dt>Sensor</dt>
+            <dd>{scene.sensor || ''}</dd>
+            <dt>Provider</dt>
+            <dd>{scene.provider}</dd>
+            <dt>Acquisition Date</dt>
+            <dd>{dateFormat(scene.acquisition_start)} - {dateFormat(scene.acquisition_end)}</dd>
+            { scene.tms ? [
+              <dt>Tile service</dt>,
+              <dd>{scene.tms}</dd>
+              ] : '' }
+            <dt>Contact</dt>
+            <dd><span className="name">{scene.contact.name}</span> <span className="email">{scene.contact.email}</span></dd>
+          </dl>
+
+          {scene.images.map(this.renderImage.bind(this))}
+
+        </div>
+        <footer className="panel-footer"></footer>
+      </section>
+      )
+  },
+
+  renderImage: function (image, i) {
+    var status;
+    var messages = image.messages.map(function (msg) { return <li>{msg}</li> });
+    if (image.status === 'finished') {
+      status = 'status-success'
+      messages.unshift(<li><a href="#" title="View image on OpenAerialMap" className="bttn-view-image">View image</a></li>)
+    } else if (image.status === 'processing') {
+      status = 'status-processing'
+      messages.unshift(<li>Upload in progress.</li>)
+    } else if (image.status === 'errored') {
+      status = 'status-error'
+      messages.unshift(<li><strong>Upload failed: </strong> {image.error.message}</li>)
+    }
+
+    status = ' ' + status + ' ';
+
+    return (
+      <div className={"image-block" + status}>
+        <h2 className="image-block-title">Image {i}</h2>
+        <p className={"status" + status}>{titlecase(image.status)}</p>
+        <dl className="status-details">
+          <dt>Started</dt>
+          <dd>{dateFormat(image.startedAt)}</dd>
+          { image.stoppedAt ? [
+            <dt>{image.status === 'finished' ? 'Finished' : 'Stopped'}</dt>,
+            <dd>{dateFormat(image.stoppedAt)}</dd>
+          ] : '' }
+          <dt>Info</dt>
+          <dd className="info-detail">
+            <ul>{messages}</ul>
+          </dd>
+        </dl>
+      </div>
+    )
   },
 
   render: function() {
@@ -56,7 +134,6 @@ var App = module.exports = React.createClass({
     }
 
     if (this.state.errored) {
-      console.log(this.state);
       return (
         <div className="intro-block">
           <h2>Status upload</h2>
@@ -66,7 +143,7 @@ var App = module.exports = React.createClass({
       );
     }
 
-    console.log('Got status:', this.state);
+    var data = this.state.data;
 
     return (
       <div>
@@ -84,72 +161,15 @@ var App = module.exports = React.createClass({
           <div className="panel-body">
             <dl className="status-details">
               <dt>Uploader</dt>
-              <dd><span className="name">Lady Stardust</span> <span className="email">lady@stardust.xyz</span></dd>
+              <dd><span className="name">{data.uploader.name}</span> <span className="email">{data.uploader.email}</span></dd>
               <dt>Date</dt>
-              <dd>2015-08-25 at 12:19</dd>
+              <dd>{dateFormat(data.createdAt)}</dd>
             </dl>
           </div>
           <footer className="panel-footer"></footer>
         </section>
 
-        <section className="panel status-panel">
-          <header className="panel-header">
-            <div className="panel-headline">
-              <h1 className="panel-title">Scene: <span className="given-title">Talisay Tanauan</span></h1>
-            </div>
-          </header>
-          <div className="panel-body">
-            <dl className="status-details">
-              <dt>Platform</dt>
-              <dd>UAV</dd>
-              <dt>Sensor</dt>
-              <dd>Blue Bird Drone 4K</dd>
-              <dt>Provider</dt>
-              <dd>SkyEye Inc.</dd>
-              <dt>Acquisition Date</dt>
-              <dd>2015-08-25 at 12:19 - 2015-08-25 at 12:19</dd>
-              <dt>Tile service</dt>
-              <dd>http://a.tiles.mapbox.com/v4/openroads.n6p4np8h</dd>
-              <dt>Contact</dt>
-              <dd><span className="name">Edgar Ilaga</span> <span className="email">edgarilaga@skyeyeproject.com</span></dd>
-            </dl>
-
-            <div className="image-block status-error">
-              <h2 className="image-block-title">Image 1</h2>
-              <p className="status status-error">Failed</p>
-              <dl className="status-details">
-                <dt>Started</dt>
-                <dd>2015-08-24 at 21:08</dd>
-                <dt>Info</dt>
-                <dd className="info-detail"><strong>Upload failed:</strong> Error message lorem ipsum dolor sit amet.</dd>
-              </dl>
-            </div>
-
-            <div className="image-block status-processing">
-              <h2 className="image-block-title">Image 2</h2>
-              <p className="status status-processing">Processing</p>
-              <dl className="status-details">
-                <dt>Started</dt>
-                <dd>2015-08-24 at 21:08</dd>
-                <dt>Info</dt>
-                <dd className="info-detail">Upload in progress.</dd>
-              </dl>
-            </div>
-
-            <div className="image-block status-success">
-              <h2 className="image-block-title">Image 3</h2>
-              <p className="status status-success">Uploaded</p>
-              <dl className="status-details">
-                <dt>Started</dt>
-                <dd>2015-08-24 at 21:08</dd>
-                <dt>Info</dt>
-                <dd className="info-detail"><a href="#" title="View image on OpenAerialMap" className="bttn-view-image">View image</a></dd>
-              </dl>
-            </div>
-
-          </div>
-          <footer className="panel-footer"></footer>
-        </section>
+        {data.scenes.map(function (scene) { return this.renderScene(scene) }.bind(this))}
 
       </div>
     );
