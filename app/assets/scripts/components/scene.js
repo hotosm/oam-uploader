a0333cc03d0bf0106ec7f19ef7c100882e782ea9
Nick Bumbarger <nbumbarger@gmail.com>
2016-12-05 18:26:35 -0500

    Change 'Upload' button text to 'Local File'

--- app/assets/scripts/components/scene.js
+++ app/assets/scripts/components/scene.js
@@ -295,7 +295,7 @@ Please check the instructions on how to use files from Google Drive.
               />
             ))}
             <div className='imagery-location-import'>
-              <button type='button' className='bttn-imagery-upload' onClick={() => this.addImageryLocation('upload')} title='Upload file directly'><span>Upload</span></button>
+              <button type='button' className='bttn-imagery-upload' onClick={() => this.addImageryLocation('upload')} title='Upload file directly'><span>Local File</span></button>
               <button type='button' className='bttn-imagery-manual' onClick={() => this.addImageryLocation('manual')} title='Write url'><span>Url</span></button>
               <button type='button' className='bttn-imagery-dropbox' onClick={this.importDropboxClick} title='Import file from dropbox'><span>Dropbox</span></button>
               <button type='button' className='bttn-imagery-gdrive' onClick={this.importGDriveClick} title='Import file from Google Drive'><span>Drive</span></button>
