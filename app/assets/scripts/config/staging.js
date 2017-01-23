803690fd5df1efc7142fc3117b0a878c1fa3d4f0
Nate Smith <nateasmith@gmail.com>
2017-01-23 09:32:56 +0000

    Update staging.js

--- app/assets/scripts/config/staging.js
+++ app/assets/scripts/config/staging.js
@@ -5,7 +5,7 @@
 // set staging-specific options here.
 module.exports = {
   environment: 'staging',
-  OAMUploaderApi: 'http://54.174.41.157/',
+  OAMUploaderApi: 'http://52.91.218.109/',
   googleClient: '36015894456-3d5ka80qtpaqcjhco3lsl38s1fj0dr71.apps.googleusercontent.com',
   googleDeveloperKey: ''
 };
